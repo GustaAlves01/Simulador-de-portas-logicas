903cb186fdba47420f2cff4a54c6e4e32316ace0
Gustavo <gustavoalima14057@gmail.com>
2026-03-21 00:02:33 -0300

    calculadora binaria para codigo

--- two-complement.c
+++ two-complement.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
+#include <stdbool.h>
+// calculadora binaria bit-bit aplicando tecnicas de abstracao
+// misturando conhecimentos de portas logicas com conhecimentos de abstracao
 
-int main() {
+bool se_complemento(int a, int b, int c);
+bool soma_binario(int a, int b, int c);
+void resultado(int bit);
 
-    int a = 6;
-    for (int i = 3; i >= 0; i--) {
-        printf("%d", ((a>>i) & 1)^1);
+void inteiro_binario(int a, int b){
+    int bin1[5], bin2[5];
+    bool carry = 0;
+
+    /*
+    4 bits apenas, i representa qual bit o
+    operador bitwise (>>) vai printar,
+    movendo cada bit individualmente
+    */
+
+    for (int i = 0; i <= 4; i++){
+        bin1[i] = (a >> i) & 1;
+        bin2[i] = (b >> i) & 1;
+
+        int soma = soma_binario(bin1[i], bin2[i], carry);
+        carry = se_complemento(bin1[i], bin2[i], carry);
+        resultado(soma);
     }
-
-    printf("\n");
-    printf("%d", sizeof(a));
-    return 0;
 }
 
+bool se_complemento(int a, int b, int c){
+    /* Formula para complemento (carry):
+       ((A & B) || ((A ^ B) & C))
+       carry deve ser 0 na primeira execucao
+    */
+    return ((a & b) || ((a ^ b) & c));
+}
 
+bool soma_binario(int a, int b, int c){
+    /* Formula para soma:
+       ((A ^ B) ^ C)
+    */
+    return ((a ^ b) ^ c);
+}
+
+void resultado(int bit){
+    printf("%d", bit);
+}
+
+int main(){
+    inteiro_binario(1,4);
+
+    return 0;
+}
